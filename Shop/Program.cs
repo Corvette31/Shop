ef9468e078df856927544bf7f7e2c624ff2bccd7
Corvette31 <corvette.ss@yandex.ru>
2022-06-12 22:27:49 +0300

    Добавьте файлы проекта.

---
+++ Shop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playersGold;
+            int playersCrystal;
+            int costCrystal = 10;
+
+            Console.Write("Сколько у вас золотых монет? :");
+            playersGold = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Сколько кристалов вы хотите приобрести по цене {costCrystal} монет за кристал ? :");
+            playersCrystal = Convert.ToInt32(Console.ReadLine());
+            playersGold -= playersCrystal * costCrystal;
+            Console.WriteLine($"После сделки у вас осталось {playersGold} золота и {playersCrystal} кристалов");
+        }
+    }
+}
